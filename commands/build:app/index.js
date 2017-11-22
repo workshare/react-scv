c91240f4996292036880e865587797f0e947933b
Marcello Montemagno <marcello.montemagno@workshare.com>
2017-11-22 11:16:33 +0000

    separating the build of the app and the build of the umd

--- commands/build:app/index.js
+++ commands/build:app/index.js
@@ -7,7 +7,6 @@ const fs = require('fs');
 const CWD = process.cwd();
 const PACKAGE = require(path.join(CWD, 'package.json'));
 const APP_BUILD_ENTRY = path.join(CWD, PACKAGE["react-scv"].appBuildEntry);
-const UMD_BUILD_ENTRY = path.join(CWD, PACKAGE["react-scv"].umdBuildEntry);
 const middleware = require('../../src/middleware');
 
 module.exports = (args, done) => {
@@ -15,19 +14,10 @@ module.exports = (args, done) => {
   process.on('SIGINT', done);
 
   return buildProductionDllIfNotPresent()
-  .then(buildUMD)
   .then(buildApp)
   .then(done);
 
 };
-
-function buildUMD () {
-  if (fs.existsSync(UMD_BUILD_ENTRY)) {
-    console.log(' --- building the umd ---');
-    const config = middleware.applyMiddleware(require.resolve('../../config/webpack.umd'));
-    return webpackBuild(config);
-  }
-}
 
 function buildApp () {
   if (fs.existsSync(APP_BUILD_ENTRY)) {
