4f6ed424ce0ffc9a87b3bd3538bcd51fcf1014f1
Boris Popik <popik.boris@gmail.com>
2019-01-18 09:51:37 +0000

    Modified rootDir in jest.js according to documentation, ignore Ts definition files

--- config/jest.js
+++ config/jest.js
@@ -11,12 +11,30 @@ const BABEL_POLYFILL = require.resolve('@babel/polyfill')
 const ROOT = path.join(CWD, 'tests');
 const SRC = path.join(CWD, 'src');
 
+/*
+From Jest documentation:
+rootDir [string]
+Default: The root of the directory containing your jest's config file or the package.json or the pwd if no package.json is found
+
+The root directory that Jest should scan for tests and modules within. If you put your Jest config inside your package.json and want the root directory to be the root of your repo, the value for this config param will default to the directory of the package.json.
+
+Oftentimes, you'll want to set this to 'src' or 'lib', corresponding to where in your repository the code is stored.
+
+roots [array]
+Default: ["<rootDir>"]
+
+A list of paths to directories that Jest should use to search for files in.
+
+There are times where you only want Jest to search in a single sub-directory (such as cases where you have a src/ directory in your repo), but prevent it from accessing the rest of the repo.
+*/
+
+
 module.exports = {
   verbose: true,
-  rootDir: ROOT,
+  rootDir: CWD,
   roots: [SRC, ROOT],
   transform: {".*": JEST_BABEL_TRANSFORMER},
-  collectCoverageFrom: ["src/**/*.(t|j)s?(x)", "!**/node_modules/**", "!**/vendor/**"],
+  collectCoverageFrom: ["src/**/*.(t|j)s?(x)", "!**/node_modules/**", "!**/vendor/**", "!src/**/*.d.ts"],
   testMatch: ['**/__tests__/**/*.(t|j)s?(x)', '**/?(*.)(spec|test).(t|j)s?(x)', '**/*+(_spec|_test).(t|j)s?(x)'],
   moduleDirectories: [CWD_NODE_MODULES, NODE_MODULES],
   moduleNameMapper: {
