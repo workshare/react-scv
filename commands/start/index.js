463a50564ea89e6c69fe78f9cf1c47815832562d
Boris.Popik <Boris.Popik@LN1-PL-2121>
2019-05-16 20:37:28 +0100

    Dependency update

--- commands/start/index.js
+++ commands/start/index.js
@@ -1,59 +1,53 @@
-'use strict';
+"use strict";
 
-const DevServer = require('webpack-dev-server');
-const path = require('path');
-const FlowStatusWebpackPlugin = require('flow-status-webpack-plugin');
+const DevServer = require("webpack-dev-server");
+const path = require("path");
 const CWD = process.cwd();
-const PACKAGE = require(path.join(CWD, 'package.json'));
+const PACKAGE = require(path.join(CWD, "package.json"));
 const devServer = PACKAGE["react-scv"].devServer || {};
 const port = devServer.port;
 const flow = devServer.flow;
-const FLOW_EXE = path.join(CWD, 'node_modules/.bin/flow');
-const FLOW_TARGET = path.join(CWD, '/node_modules/react-scv/config/');
-const webpack = require('webpack');
-const middleware = require('../../src/middleware');
-const chalk = require('chalk');
+const FLOW_EXE = path.join(CWD, "node_modules/.bin/flow");
+const FLOW_TARGET = path.join(CWD, "/node_modules/react-scv/config/");
+const webpack = require("webpack");
+const middleware = require("../../src/middleware");
+const chalk = require("chalk");
 
 module.exports = (args, done) => {
-
   if (!port) {
-    throw new Error('Please specify a port for the dev server in your package.json => src.devServer.port property');
+    throw new Error(
+      "Please specify a port for the dev server in your package.json => src.devServer.port property"
+    );
   }
 
-  const config = middleware.applyMiddleware(require.resolve('../../config/webpack.dev'));
+  const config = middleware.applyMiddleware(
+    require.resolve("../../config/webpack.dev")
+  );
 
-  const schema = config.devServer.https ? 'https' : 'http';
-  const host = config.devServer.host || 'localhost';
+  const schema = config.devServer.https ? "https" : "http";
+  const host = config.devServer.host || "localhost";
 
   config.entry.unshift(
     "react-hot-loader/patch",
     `webpack-dev-server/client?${schema}://${host}:${port}`,
-    'webpack/hot/dev-server'
+    "webpack/hot/dev-server"
   );
 
-  console.log(chalk.blue('≡ ') + chalk.magenta('Starting dev server and API proxy') + chalk.blue(' ≡'));
-
-  //todo experimental feature
-  if (flow) {
-    config.plugins.push(new FlowStatusWebpackPlugin({
-      root: FLOW_TARGET,
-      binaryPath: FLOW_EXE,
-      flowArgs: ' --include ' + CWD,
-      failOnError: true
-    }));
-  }
+  console.log(chalk.magenta("■ Starting dev server and API proxy ■"));
 
   const compiler = webpack(config);
 
-  config.output.publicPath = schema + "://" + host + ":" + port + config.output.publicPath;
+  config.output.publicPath =
+    schema + "://" + host + ":" + port + config.output.publicPath;
 
   const server = new DevServer(compiler, config.devServer);
 
-  server.listen(port, host, () => console.log(`Listening on ` + chalk.blue(`${schema}://${host}:${port}`)));
+  server.listen(port, host, () =>
+    console.log(`Listening on ` + chalk.blue(`${schema}://${host}:${port}`))
+  );
 
-  process.on('SIGINT', () => {
+  process.on("SIGINT", () => {
     server.close();
     done();
   });
-
 };
