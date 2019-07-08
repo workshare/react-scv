f68b56ce7281da3efb4f5639987312f9e9936325
Marcello Montemagno <marcello.montemagno@workshare.com>
2019-07-08 15:21:03 +0100

    removing flow experimental feature

--- commands/start/index.js
+++ commands/start/index.js
@@ -2,14 +2,10 @@
 
 const DevServer = require('webpack-dev-server');
 const path = require('path');
-const FlowStatusWebpackPlugin = require('flow-status-webpack-plugin');
 const CWD = process.cwd();
 const PACKAGE = require(path.join(CWD, 'package.json'));
 const devServer = PACKAGE["react-scv"].devServer || {};
 const port = devServer.port;
-const flow = devServer.flow;
-const FLOW_EXE = path.join(CWD, 'node_modules/.bin/flow');
-const FLOW_TARGET = path.join(CWD, '/node_modules/react-scv/config/');
 const webpack = require('webpack');
 const middleware = require('../../src/middleware');
 
@@ -31,16 +27,6 @@ module.exports = (args, done) => {
   );
 
   console.log(' --- starting dev server, and api proxy --- ');
-
-  //todo experimental feature
-  if (flow) {
-    config.plugins.push(new FlowStatusWebpackPlugin({
-      root: FLOW_TARGET,
-      binaryPath: FLOW_EXE,
-      flowArgs: ' --include ' + CWD,
-      failOnError: true
-    }));
-  }
 
   const compiler = webpack(config);
 
